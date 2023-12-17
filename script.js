1177e60169dc1f240f92dfb2eae95aea8f117f00
NingChenMeta <153161682+NingChenMeta@users.noreply.github.com>
2023-12-17 11:54:18 +0800

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.getElementById("nextButton").addEventListener("click", function () {
-  fetch("article.txt")
-    .then((response) => response.text())
-    .then((text) => {
-      let articleParts = text.split("\n\n"); // 假设每个段落由两个换行符分隔
-      displayNextPart(articleParts);
-    });
+document.getElementById('nextButton').addEventListener('click', function() {
+    fetch('article.txt')
+        .then(response => response.text())
+        .then(text => {
+            let articleParts = text.split("\n\n"); // 假设每个段落由两个换行符分隔
+            displayNextPart(articleParts);
+        });
 });
 
 let currentPart = 0;
@@ -12,18 +12,23 @@ let isArticleLoaded = false;
 let articleParts = [];
 
 function displayNextPart(parts) {
-  if (!isArticleLoaded) {
-    articleParts = parts;
-    isArticleLoaded = true;
-  }
+    if (!isArticleLoaded) {
+        articleParts = parts;
+        isArticleLoaded = true;
+    }
 
-  if (currentPart < articleParts.length) {
-    let currentText = document.getElementById("dialogText").innerHTML;
-    document.getElementById("dialogText").innerHTML =
-      currentText + "<p>" + articleParts[currentPart] + "</p>";
-    currentPart++;
-  } else {
-    document.getElementById("nextButton").innerText = "已结束";
-    document.getElementById("nextButton").disabled = true;
-  }
+    if (currentPart < articleParts.length) {
+        let dialogBox = document.getElementById('dialogBox');
+        let newParagraph = document.createElement("p");
+        newParagraph.innerText = articleParts[currentPart];
+        dialogBox.appendChild(newParagraph);
+
+        // 滚动到对话框的底部
+        dialogBox.scrollTop = dialogBox.scrollHeight;
+
+        currentPart++;
+    } else {
+        document.getElementById('nextButton').innerText = "已结束";
+        document.getElementById('nextButton').disabled = true;
+    }
 }
