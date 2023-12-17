51a4c0d53a22328a07ba05fee27eb2ce73834ecb
NingChenMeta <153161682+NingChenMeta@users.noreply.github.com>
2023-12-17 12:07:28 +0800

    script.js

--- script.js
+++ script.js
@@ -19,18 +19,29 @@ function displayNextPart(parts) {
 
     if (currentPart < articleParts.length) {
         let dialogBox = document.getElementById('dialogBox');
-        let newParagraph = document.createElement("p");
-        newParagraph.innerText = articleParts[currentPart];
+        let newParagraph = document.createElement("div");
+        newParagraph.classList.add("dialog-entry");
+
+        // 创建头像元素
+        let avatar = document.createElement("div");
+        avatar.classList.add("avatar");
+
+        // 创建文本元素
+        let text = document.createElement("p");
+        text.innerText = articleParts[currentPart];
+
+        newParagraph.appendChild(avatar);
+        newParagraph.appendChild(text);
         dialogBox.appendChild(newParagraph);
+
+        // 滚动到对话框的底部
+        setTimeout(() => {
+            dialogBox.scrollTop = dialogBox.scrollHeight;
+        }, 0);
 
         currentPart++;
     } else {
         document.getElementById('nextButton').innerText = "已结束";
         document.getElementById('nextButton').disabled = true;
     }
-
-    // 更新滚动位置，将其移至对话框的底部
-    setTimeout(() => {
-        dialogBox.scrollTop = dialogBox.scrollHeight;
-    }, 0);
 }
