18f01a8c20a1066619d0dea1743a2f88ad3b66a5
NingChenMeta <153161682+NingChenMeta@users.noreply.github.com>
2023-12-17 13:47:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-document.getElementById("nextButton").addEventListener("click", function () {
-  fetch("article.txt")
+document.getElementById("nextButton").addEventListener("click", () => {
+  fetch("article.txt") // 假设文章内容存储在服务器上的 article.txt 文件中
     .then((response) => response.text())
     .then((text) => {
-      let articleParts = text.split("\n\n"); // 假设每个段落由两个换行符分隔
-      displayNextPart(articleParts);
+      displayNextPart(text);
     });
 });
 
@@ -11,19 +10,44 @@ let currentPart = 0;
 let isArticleLoaded = false;
 let articleParts = [];
 
-function displayNextPart(parts) {
+function displayNextPart(text) {
   if (!isArticleLoaded) {
-    articleParts = parts;
+    articleParts = text.split("\n\n"); // 假设每个段落由两个换行符分隔
     isArticleLoaded = true;
   }
 
   if (currentPart < articleParts.length) {
-    let currentText = document.getElementById("dialogText").innerHTML;
-    document.getElementById("dialogText").innerHTML =
-      currentText + "<p>" + articleParts[currentPart] + "</p>";
+    const chatBox = document.getElementById("chatBox");
+    const chatMessage = document.createElement("div");
+    chatMessage.classList.add("chat-message");
+
+    const avatar = document.createElement("div");
+    avatar.classList.add("avatar");
+
+    const messageContent = document.createElement("div");
+    messageContent.classList.add("message-content");
+
+    const nickname = document.createElement("div");
+    nickname.classList.add("nickname");
+    nickname.innerText = "宁晨"; // 昵称文本
+
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.innerText = articleParts[currentPart];
+
+    messageContent.appendChild(nickname);
+    messageContent.appendChild(message);
+    chatMessage.appendChild(avatar);
+    chatMessage.appendChild(messageContent);
+    chatBox.appendChild(chatMessage);
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+
     currentPart++;
-  } else {
-    document.getElementById("nextButton").innerText = "已结束";
+  }
+
+  if (currentPart >= articleParts.length) {
     document.getElementById("nextButton").disabled = true;
+    document.getElementById("nextButton").innerText = "已结束";
   }
 }
